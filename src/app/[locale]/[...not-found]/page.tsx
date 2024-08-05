ef5fb0b33b057b7af3c2af5ef3ca8ef20cb937c5
jadj <jakub.jadczyk@innsoft.pl>
2024-08-05 15:30:07 +0200

    fix build error

--- src/app/[locale]/[...not-found]/page.tsx
+++ src/app/[locale]/[...not-found]/page.tsx
@@ -1,7 +1,8 @@
+"use client"
 import React from 'react'
 import Link from 'next/link'
 
-export default function page({ params: { locale } }: any) {
+export default function page() {
   return (
     <div className='grid justify-items-center gap-4'>
       <h2 className='font-bold text-2xl'>Not Found</h2>
