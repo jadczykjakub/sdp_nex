c6793b5bfdef76a459d1be444d402c15e95894de
jadj <jakub.jadczyk@innsoft.pl>
2024-08-05 15:27:31 +0200

    getStatic error

--- src/app/[locale]/[...not-found]/page.tsx
+++ src/app/[locale]/[...not-found]/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 
-export default function page() {
+export default function page({ params: { locale } }: any) {
   return (
     <div className='grid justify-items-center gap-4'>
       <h2 className='font-bold text-2xl'>Not Found</h2>
