10236874bb0c24faaf3777b45ad09409a4a877aa
jadj <jakub.jadczyk@innsoft.pl>
2024-08-05 15:38:04 +0200

    test omit error

--- src/app/[locale]/[...not-found]/page.tsx
+++ src/app/[locale]/[...not-found]/page.tsx
@@ -1,8 +1,18 @@
-"use client"
 import React from 'react'
 import Link from 'next/link'
+import { notFound } from 'next/navigation'
+import { getBlogs } from '@/../lib/get-blogs'
+import type { Metadata } from 'next'
 
-export default function page() {
+export async function generateMetadata({
+  params,
+}: {
+  params: any
+}): Promise<any> {
+  const blog = getBlogs(params.locale).find((post) => post.slug === params.slug)
+}
+
+export default function page({ params: { locale } }: any) {
   return (
     <div className='grid justify-items-center gap-4'>
       <h2 className='font-bold text-2xl'>Not Found</h2>
