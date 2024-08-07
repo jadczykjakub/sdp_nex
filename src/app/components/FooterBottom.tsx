e2d37ae28dcd1bbbe9946ccc94af89d1925a6da3
jadj <jakub.jadczyk@innsoft.pl>
2024-08-07 13:28:08 +0200

    try fix images

--- src/app/components/FooterBottom.tsx
+++ src/app/components/FooterBottom.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import footerLogoImage from '@/../public/images/layout/logo_footer.jpg'
 import Image from 'next/image';
 import { useTranslation } from 'react-i18next';
 import Link from 'next/link';
@@ -10,7 +9,7 @@ const FooterBottom = () => {
     const { t, i18n } = useTranslation();
 
     return (<div className='flex flex-col items-center text-center  gap-4  md:flex-row md:items-center md:justify-between px-5 md:px-14 layout'>
-        <Image src={`./${footerLogoImage}`} alt='sdp logo image' width={100} height={60} />
+        <Image src={'./images/layout/logo_footer.jpg'} alt='sdp logo image' width={100} height={60} />
         <span>{t('bottom.copyright', {ns: 'footer'})}</span>
         <Link href="/files/privacy-policy.pdf" target='_blank'>{t('bottom.policy', {ns: 'footer'})}</Link>
     </div>);
